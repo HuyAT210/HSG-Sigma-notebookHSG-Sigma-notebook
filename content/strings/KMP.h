95b4cec20521f35437b87aa3249cf8a03df73741
HuyAT210 <huy.20080119.606@gmail.com>
2025-10-18 22:10:08 +0700

    Update lots of thing

---
+++ content/strings/KMP.h
@@ -0,0 +1,19 @@
+/**
+ * Author: Pannda
+ * Description: Longest prefix of s that ends at i.
+ * Status: Tested
+ */
+
+vector<int> getPi(string s) {
+    int n = s.size();
+    vector<int> pi(n);
+    pi[0] = 0;
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+        pi[i] = j + (s[i] == s[j]);
+    }
+    return pi;
+}
